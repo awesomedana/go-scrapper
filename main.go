608d26e08c81571320ed95d20715ad59c7e2c892
awesomedana <dana042118@gmail.com>
2021-03-08 17:48:11 +0900

    Function Part2(naked return, defer)

--- main.go
+++ main.go
@@ -5,29 +5,14 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int { //a와 b의 데이터타입이 int이고, 리턴값의 데이터타입이 int이다.
-	return a * b
-}
-
-// 2개의 리턴이 가능하다!!
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done.") // defer는 func이 리턴한 후에 실행된다!
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return // naked return: 리턴 뒤에 변수를 지정해주지 않아도 된다!
 }
 
 func main() {
-	// var name string = "hayeon"
-	name := "hayeon" // 위에거랑 같음 but short. 여기서 처음 설정한 타입이 계속 적용된다.(String으로 지정했다면 끝까지 String)
-	name = "Dana"
-	fmt.Println(name)
-
-	fmt.Println(multiply(2, 2))
-
-	totalLength, upperName := lenAndUpper("hayeon")
-	fmt.Println(totalLength, upperName)
-
-	repeatMe("Hayeon", "Dana", "Harley", "Mike")
+	totalLength, up := lenAndUpper("hayeon")
+	fmt.Println(totalLength, up)
 }

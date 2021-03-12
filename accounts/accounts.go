d7084e27dbc420a01cd844265e6cfdc997d44938
awesomedana <dana042118@gmail.com>
2021-03-12 22:44:25 +0900

    ChangeOwner, Owner, String method

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,6 +1,9 @@
 package accounts
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // Account struct (Account로 시작하는 코멘트가 있어야 error가 뜨지 않는다.)
 type Account struct {
@@ -34,4 +37,18 @@ func (a *Account) Withdraw(amount int) error {
 	}
 	a.balance -= amount
 	return nil
+}
+
+// ChangeOwner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Owner of the account
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account. \nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/awesomedana/go-scrapper/accounts"
 )
@@ -11,9 +10,10 @@ func main() {
 	account := accounts.NewAccount("dana")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
-	err := account.Withdraw(12)
-	if err != nil {
-		log.Fatalln(err) // println and kill the program
-	}
-	fmt.Println(account.Balance())
+	// err := account.Withdraw(2)
+	// if err != nil {
+	// 	log.Fatalln(err) // println and kill the program
+	// }
+	fmt.Println(account.Balance(), account.Owner())
+	fmt.Println(account)
 }
